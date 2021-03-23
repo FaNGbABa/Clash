7e5dfc60fbd9e7f479a434bacbf3dd7a7f61170c
Gregory <40469717+FaNGbABa@users.noreply.github.com>
2021-03-23 17:48:32 +0800

    Update birthday.js

--- Quantumult X/Task/birthday.js
+++ Quantumult X/Task/birthday.js
@@ -18,7 +18,7 @@ const title = "🐣破壳日🐣";
 
 $.config = {
   username: "房佳伟", // 姓名
-  birthday: "1998.07.04", // 生日日期
+  birthday: "07.04", // 生日日期
   physiologicalDefault: "", // 最近一次来周期时间
   physiologicalCycle: "", // 下一次周期
   nongli: true, // 农历生日
